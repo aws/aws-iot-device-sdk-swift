6332e94117574255bc9378219ba439ddb8b148a3
Steve Kim <sbstevek@amazon.com>
2024-12-20 15:03:44 -0800

    remaining properties for builder

--- Sources/AwsIotDeviceSdkSwift/mqtt5ClientBuilder.swift
+++ Sources/AwsIotDeviceSdkSwift/mqtt5ClientBuilder.swift
@@ -25,23 +25,23 @@ fileprivate func getMetricsStr(currentUsername: String = "") -> String {
 
 public class Mqtt5ClientBuilder {
 
-    var _endpoint: String? = nil
-    var _port: UInt32 = 8883
-    var _tlsCtx: TLSContext? = nil
-    var _onWebsocketTransform: OnWebSocketHandshakeIntercept? = nil
-    var _clientId: String? = nil
-    var _username: String? = nil
-    var _password: Data? = nil
-    var _keepAliveInterval: TimeInterval = 1200
-    var _sessionExpiryInterval: TimeInterval? = nil
-    var _extendedValidationAndFlowControlOptions: ExtendedValidationAndFlowControlOptions? = .awsIotCoreDefaults
-    var _onPublishReceived: OnPublishReceived? = nil
-    var _onLifecycleEventAttemptingConnect: OnLifecycleEventAttemptingConnect? = nil
-    var _onLifecycleEventConnectionSuccess: OnLifecycleEventConnectionSuccess? = nil
-    var _onLifecycleEventConnectionFailure: OnLifecycleEventConnectionFailure? = nil
-    var _onLifecycleEventDisconnection: OnLifecycleEventDisconnection? = nil
-    var _onLifecycleEventStopped: OnLifecycleEventStopped? = nil
-    var _enableMetricsCollection: Bool = true
+    private var _endpoint: String? = nil
+    private var _port: UInt32 = 8883
+    private var _tlsCtx: TLSContext? = nil
+    private var _onWebsocketTransform: OnWebSocketHandshakeIntercept? = nil
+    private var _clientId: String? = nil
+    private var _username: String? = nil
+    private var _password: Data? = nil
+    private var _keepAliveInterval: TimeInterval = 1200
+    private var _sessionExpiryInterval: TimeInterval? = nil
+    private var _extendedValidationAndFlowControlOptions: ExtendedValidationAndFlowControlOptions? = .awsIotCoreDefaults
+    private var _onPublishReceived: OnPublishReceived? = nil
+    private var _onLifecycleEventAttemptingConnect: OnLifecycleEventAttemptingConnect? = nil
+    private var _onLifecycleEventConnectionSuccess: OnLifecycleEventConnectionSuccess? = nil
+    private var _onLifecycleEventConnectionFailure: OnLifecycleEventConnectionFailure? = nil
+    private var _onLifecycleEventDisconnection: OnLifecycleEventDisconnection? = nil
+    private var _onLifecycleEventStopped: OnLifecycleEventStopped? = nil
+    private var _enableMetricsCollection: Bool = true
 
     // mtlsFromPath
     init (certPath: String, keyPath: String, endpoint: String, port: UInt32 = 8883) throws {
@@ -189,40 +189,6 @@ public class Mqtt5ClientBuilder {
                                       credentialsProvider: credentialsProvider)
     }
 
-    /// Custom Auth
-
-    /// Creates and returns an Mqtt5ClientBuilder configured for an MQTT5 Client using a custom authorizer
-    /// 
-    /// - Parameters:
-    ///   - endpoint: Host name of AWS IoT server.
-    ///   - port: Override default server port.
-    ///     Default port is 443 if system supports ALPN or websockets are being used.
-    ///     Otherwise, default port is 8883.
-    ///   - authUsername: The username to use with the custom authorizer.
-    ///     If provided, the username given will be passed when connecting to the custom authorizer.
-    ///     If not provided, it will check to see if a username has already been set (via username="example")
-    ///     and will use that instead.  Custom authentication parameters will be appended as appropriate
-    ///     to any supplied username value.
-    ///   - authPassword: The password to use with the custom authorizer.
-    ///     If not provided, then no password will be sent in the initial CONNECT packet.
-    ///   - authAuthorizerName: Name of the custom authorizer to use.
-    ///     Required if the endpoint does not have a default custom authorizer associated with it.  It is strongly
-    ///     suggested to URL-encode this value; the SDK will not do so for you.
-    ///   - authAuthorizerSignature: The digital signature of the token value in the `auth_token_value`
-    ///     parameter. The signature must be based on the private key associated with the custom authorizer.  The
-    ///     signature must be base64 encoded.
-    ///     Required if the custom authorizer has signing enabled.
-    ///   - authTokenKeyName: Key used to extract the custom authorizer token from MQTT username query-string
-    ///     properties.
-    ///     Required if the custom authorizer has signing enabled.  It is strongly suggested to URL-encode
-    ///     this value; the SDK will not do so for you.
-    ///   - authTokenValue: An opaque token value. This value must be signed by the private key associated with
-    ///     the custom authorizer and the result passed in via the `auth_authorizer_signature` parameter.
-    ///     Required if the custom authorizer has signing enabled.
-    /// - Throws: 
-    /// - Returns: an Mqtt5ClientBuilder configured for an MQTT5 Client using a custom authorizer
-
-
     // Helper function to append parameters to username
     fileprivate func appendToUsernameParameter(inputString: String, parameterValue: String, parameterPretext: String) -> String {
         var returnString = inputString
@@ -316,92 +282,6 @@ public class Mqtt5ClientBuilder {
                                       useWebsocket: false)
     }
 
-    /*
-    **client_options** (:class:`awscrt.mqtt5.ClientOptions`): This dataclass can be used to to apply all
-            configuration options for Client creation. Any options set within will supercede defaults
-            assigned by the builder. Any omitted arguments within this class will be filled by additional
-            keyword arguments provided to the builder or be set to their default values.
-
-    **connect_options** (:class:`awscrt.mqtt5.ConnectPacket`): This dataclass can be used to apply connection
-            options for the client. Any options set within will supercede defaults assigned by the builder but
-            will not overwrite options set by connect_options included within a client_options keyword argument.
-            Any omitted arguments within this class will be assigned values of keyword arguments provided to
-            the builder.
-
-    **client_bootstrap** (:class:`awscrt.io.ClientBootstrap`): Client bootstrap used to establish connection.
-        The ClientBootstrap will default to the static default (Io.ClientBootstrap.get_or_create_static_default)
-        if the argument is omitted or set to 'None'.
-
-    **http_proxy_options** (:class:`awscrt.http.HttpProxyOptions`): HTTP proxy options to use
-
-    **request_response_information** (`bool`): If true, requests that the server send response information in
-        the subsequent CONNACK.  This response information may be used to set up request-response implementations
-        over MQTT, but doing so is outside the scope of the MQTT5 spec and client.
-
-    **request_problem_information** (`bool`): If true, requests that the server send additional diagnostic
-        information (via response string or user properties) in DISCONNECT or CONNACK packets from the server.
-
-    **receive_maximum** (`int`): Notifies the server of the maximum number of in-flight QoS 1 and 2 messages the
-        client is willing to handle.  If omitted or null, then no limit is requested.
-
-    **maximum_packet_size** (`int`): Notifies the server of the maximum packet size the client is willing to handle.
-        If omitted or null, then no limit beyond the natural limits of MQTT packet size is requested.
-
-    **will_delay_interval_sec** (`int`): A time interval, in seconds, that the server should wait (for a session
-        reconnection) before sending the will message associated with the connection's session.  If omitted or
-        null, the server will send the will when the associated session is destroyed.  If the session is destroyed
-        before a will delay interval has elapsed, then the will must be sent at the time of session destruction.
-
-    **will** (:class:`awscrt.mqtt5.PublishPacket`): The definition of a message to be published when the connection's
-        session is destroyed by the server or when the will delay interval has elapsed, whichever comes first.  If
-        null, then nothing will be sent.
-
-    **user_properties** (`Sequence` [:class:`awscrt.mqtt5.UserProperty`]): List of MQTT5 user properties included
-        with the packet.
-
-    **session_behavior** (:class:`awscrt.mqtt5.ClientSessionBehaviorType`): How the MQTT5 client should behave with
-        respect to MQTT sessions.
-
-
-
-    **offline_queue_behavior** (:class:`awscrt.mqtt5.ClientOperationQueueBehaviorType`): Returns how disconnects
-        affect the queued and in-progress operations tracked by the client.  Also controls how new operations are
-        handled while the client is not connected.  In particular, if the client is not connected, then any operation
-        that would be failed on disconnect (according to these rules) will also be rejected.
-
-    **topic_aliasing_options** (:class:`awscrt.mqtt5.TopicAliasingOptions`): Configuration options for how the client
-        should use the topic aliasing features of MQTT5
-
-    **retry_jitter_mode** (:class:`awscrt.mqtt5.ExponentialBackoffJitterMode`): How the reconnect delay is modified
-        in order to smooth out the distribution of reconnection attempt timepoints for a large set of reconnecting
-        clients.
-
-    **min_reconnect_delay_ms** (`int`): The minimum amount of time to wait to reconnect after a disconnect.
-        Exponential backoff is performed with jitter after each connection failure.
-
-    **max_reconnect_delay_ms** (`int`): The maximum amount of time to wait to reconnect after a disconnect.
-    Exponential backoff is performed with jitter after each connection failure.
-
-    **min_connected_time_to_reset_reconnect_delay_ms** (`int`): The amount of time that must elapse with an
-        established connection before the reconnect delay is reset to the minimum. This helps alleviate
-        bandwidth-waste in fast reconnect cycles due to permission failures on operations.
-
-    **ping_timeout_ms** (`int`): The time interval to wait after sending a PINGREQ for a PINGRESP to arrive. If one
-        does not arrive, the client will close the current connection.
-
-    **connack_timeout_ms** (`int`): The time interval to wait after sending a CONNECT request for a CONNACK to arrive.
-        If one does not arrive, the connection will be shut down.
-
-    **ack_timeout_sec** (`int`): The time interval to wait for an ack after sending a QoS 1+ PUBLISH, SUBSCRIBE,
-        or UNSUBSCRIBE before failing the operation.
-
-    **ca_filepath** (`str`): Override default trust store with CA certificates from this PEM formatted file.
-
-    **ca_dirpath** (`str`): Override default trust store with CA certificates loaded from this directory (Unix only).
-
-    **ca_bytes** (`bytes`): Override default trust store with CA certificates from these PEM formatted bytes.
-    */
-
     /// Set callbacks for MQTT5 Client. 
     ///
     /// - Parameters:
@@ -449,15 +329,6 @@ public class Mqtt5ClientBuilder {
     public func withOnLifecycleEventStopped(_ onLifecycleEventStopped: OnLifecycleEventStopped?) {
         _onLifecycleEventStopped = onLifecycleEventStopped
     }
-
-    // WIP DEBUG check whether we want to allow anyone to turn this off.
-    /// Whether to send the SDK version number in the CONNECT packet.
-    /// Default is True.
-    ///
-    /// - Parameter enableMetricsCollection: (Bool)
-    // public func withMetricsCollection(_ enableMetricsCollection: Bool) {
-    //     _enableMetricsCollection = enableMetricsCollection
-    // }
 
     /// **port** (`int`): Override default server port.
     /// Default port is 443 if system supports ALPN or websockets are being used.
@@ -523,6 +394,120 @@ public class Mqtt5ClientBuilder {
         _extendedValidationAndFlowControlOptions = flowControlOptions
     }
 
+    // DEBUG WIP we need to make sure the CA is being set properly in tls ctx
+    private var _caPath: String? = nil
+    public func withCaPath(_ caPath: String) {
+        _caPath = caPath
+    }
+
+    private var _caDirPath: String? = nil
+    public func withCaDirPath(_ caDirPath: String) {
+        _caDirPath = caDirPath
+    }
+
+    private var _caData: Data? = nil
+    public func withCaData(_ caData: Data) {
+        _caData = caData
+    }
+
+    private var _ackTimeout: TimeInterval? = nil
+    public func withAckTimeout(_ ackTimeout: TimeInterval) {
+        _ackTimeout = ackTimeout
+    }
+
+    private var _connackTimeout: TimeInterval? = nil
+    public func withConnackTimeout(_ connackTimeout: TimeInterval) {
+        _connackTimeout = connackTimeout
+    }
+
+    private var _pingTimeout: TimeInterval? = nil
+    public func withPingTimeout(_ pingTimeout: TimeInterval) {
+        _pingTimeout = pingTimeout
+    }
+
+    private var _minReconnectDelay: TimeInterval? = nil
+    public func withMinReconnectDelay(_ minReconnectDelay: TimeInterval) {
+        _minReconnectDelay = minReconnectDelay
+    }
+    private var _maxReconnectDelay: TimeInterval? = nil
+    public func withMaxReconnectDelay(_ maxReconnectDelay: TimeInterval) {
+        _maxReconnectDelay = maxReconnectDelay
+    }
+    private var _minConnectedTimeToResetReconnectDelay: TimeInterval? = nil
+    public func withMinConnectedTimeToResetReconnectDelay(_ minConnectedTimeToResetReconnectDelay: TimeInterval) {
+        _minConnectedTimeToResetReconnectDelay = minConnectedTimeToResetReconnectDelay
+    }
+
+    private var _retryJitterMode: ExponentialBackoffJitterMode? = nil
+    public func withRetryJitterMode(_ retryJitterMode: ExponentialBackoffJitterMode) {
+        _retryJitterMode = retryJitterMode
+    }
+
+    private var _clientOperationQueueBehaviorType: ClientOperationQueueBehaviorType? = nil
+    public func withClientOperationQueueBehaviorType(_ clientOperationQueueBehaviorType: ClientOperationQueueBehaviorType) {
+        _clientOperationQueueBehaviorType = clientOperationQueueBehaviorType
+    }
+
+    private var _clientSessionBehaviorType: ClientSessionBehaviorType? = nil
+    public func withClientSessionBehaviorType(_ clientSessionBehaviorType: ClientSessionBehaviorType) {
+        _clientSessionBehaviorType = clientSessionBehaviorType
+    }
+
+    private var _topicAliasingOptions: TopicAliasingOptions? = nil
+    public func withTopicAliasingOptions(_ topicAliasingOptions: TopicAliasingOptions) {
+        _topicAliasingOptions = topicAliasingOptions
+    }
+
+    private var _httpProxyOptions: HTTPProxyOptions? = nil
+    public func withHttyProxyOptions(_ httpProxyOptions: HTTPProxyOptions) {
+        _httpProxyOptions = httpProxyOptions
+    }
+
+    private var _socketOptions: SocketOptions? = nil
+    public func withSocketOptions(_ socketOptions: SocketOptions) {
+        _socketOptions = socketOptions
+    }
+
+    private var _clientBootstrap: ClientBootstrap? = nil
+    public func withBootstrap(_ clientBootstrap: ClientBootstrap){
+        _clientBootstrap = clientBootstrap
+    }
+
+    private var _requestResponseInformation: Bool? = nil
+    public func withRequestResponseInformation(_ requestResponseInformation: Bool) {
+        _requestResponseInformation = requestResponseInformation
+    }
+
+    private var _requestProblemInformation: Bool? = nil
+    public func withRequestProblemInformation(_ requestProblemInformation: Bool){
+        _requestProblemInformation = requestProblemInformation
+    }
+
+    private var _receiveMaximum: UInt16? = nil
+    public func withReceiveMaximum(_ receiveMaximum: UInt16) {
+        _receiveMaximum = receiveMaximum
+    }
+
+    private var _maximumPacketSize: UInt32? = nil
+    public func withMaximumPacketSize(_ maximumPacketSize: UInt32) {
+        _maximumPacketSize = maximumPacketSize
+    }
+
+    private var _willDelayInterval: TimeInterval? = nil
+    public func withWillDelayInterval(_ willDelayInterval: TimeInterval) {
+        _willDelayInterval = willDelayInterval
+    }
+
+    private var _will: PublishPacket? = nil
+    public func withWill(_ will: PublishPacket) {
+        _will = will
+    }
+
+    private var _userProperties: [UserProperty]? = nil 
+    public func withUserProperties(_ userProperties: [UserProperty]) {
+        _userProperties = userProperties
+    }
+
     public func build() throws -> Mqtt5Client {
         guard let unwrappedEndpoint = _endpoint else {
             throw AwsIotDeviceSdkError.missingParameter(parameterName: "Mqtt5ClientBuilder requires endpoint to build client.")
@@ -543,17 +528,37 @@ public class Mqtt5ClientBuilder {
             clientId: _clientId,
             username: metricsUsername,
             password: _password,
-            sessionExpiryInterval: _sessionExpiryInterval
+            sessionExpiryInterval: _sessionExpiryInterval,
+            requestResponseInformation: _requestResponseInformation,
+            requestProblemInformation: _requestProblemInformation,
+            receiveMaximum: _receiveMaximum,
+            maximumPacketSize: _maximumPacketSize,
+            willDelayInterval: _willDelayInterval,
+            will: _will,
+            userProperties: _userProperties
         )
-    
+
         // Configure client options
         let clientOptions = MqttClientOptions(
             hostName: unwrappedEndpoint,
             port: _port,
+            bootstrap: _clientBootstrap,
+            socketOptions: _socketOptions,
             tlsCtx: _tlsCtx,
             onWebsocketTransform: _onWebsocketTransform,
+            httpProxyOptions: _httpProxyOptions,
             connectOptions: connectOptions,
+            sessionBehavior: _clientSessionBehaviorType,
             extendedValidationAndFlowControlOptions: _extendedValidationAndFlowControlOptions,
+            offlineQueueBehavior: _clientOperationQueueBehaviorType,
+            retryJitterMode: _retryJitterMode,
+            minReconnectDelay: _minReconnectDelay,
+            maxReconnectDelay: _maxReconnectDelay,
+            minConnectedTimeToResetReconnectDelay: _minConnectedTimeToResetReconnectDelay,
+            pingTimeout: _pingTimeout,
+            connackTimeout: _connackTimeout,
+            ackTimeout: _ackTimeout,
+            topicAliasingOptions: _topicAliasingOptions,
             onPublishReceivedFn: _onPublishReceived,
             onLifecycleEventStoppedFn: _onLifecycleEventStopped,
             onLifecycleEventAttemptingConnectFn: _onLifecycleEventAttemptingConnect,
