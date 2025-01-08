ee9f41720f11a7680d94c62517a3196dd31df023
Vera Xia <zhvxia@amazon.com>
2025-01-08 10:08:21 -0800

    fix product name

--- Samples/ConnectSample/CognitoProviderConnectSample/Package.swift
+++ Samples/ConnectSample/CognitoProviderConnectSample/Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "X509MTLSConnectSample",
+    name: "CognitoProviderConnectSample",
         platforms: [
         .iOS(.v13), 
         .macOS(.v10_15), 
         .tvOS(.v13)
     ],
     products: [
-        .executable(name: "X509MTLSConnectSample", targets: ["X509MTLSConnectSample"])
+        .executable(name: "CognitoProviderConnectSample", targets: ["CognitoProviderConnectSample"])
     ],
     dependencies: [
         .package(path: "../../../"), // TODO: DEBUG WIP change 'branch' to `from: "aws-crt-swift version number"` when crt is updated.
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "X509MTLSConnectSample",
+            name: "CognitoProviderConnectSample",
             dependencies: [
                 .product(name: "AwsIotDeviceSdkSwift", package: "aws-iot-device-sdk-swift"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
