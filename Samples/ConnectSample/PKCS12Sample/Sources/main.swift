718939b480998a6db98da1dc6adba001a901899d
Steve Kim <sbstevek@amazon.com>
2025-02-07 11:00:58 -0800

    PKCS12 sample

--- Samples/ConnectSample/PKCS12Sample/Sources/main.swift
+++ Samples/ConnectSample/PKCS12Sample/Sources/main.swift
@@ -1,25 +1,30 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0.
 
-import Foundation
-import AwsIotDeviceSdkSwift
+// ArgumentParser is used by the sample to parse arguments.
+// This is not a required import for the MQTT5 Client.
 import ArgumentParser
 
-// This sample shows how to create an MQTT5 client and connect using a PKCS12 file.
-// Here are the steps to setup a client and connect.
-// 0. Parsing command line arguments
-// 1. Init library
-// 2. Setup Connect Options & callbacks
-// 3. Create Mqtt Client with MqttClientBuilder
-// 4. Start the connection session
-// 5. Stop the connection session
+import Foundation
+import AwsIotDeviceSdkSwift
 
-/**************************************
- * 0. Parsing command line arguments
- * Setup commandline arguments
- **************************************/
+
+// This sample creates an MQTT5 client and connects using a PKCS12 file.
+// Here are the steps to setup a client and connect.
+// 0. Sample only: Parse command line arguments
+// 1. Initialize Device Sdk library
+// 2. Create Mqtt5ClientBuilder 
+// 3. Setup Callbacks and other options
+// 4. Create an Mqtt5 Client with MqttClientBuilder
+// 5. Start the connection session
+// 6. Stop the connection session
+
+
 @main
 struct PKCS12Sample: ParsableCommand {
+    /**************************************
+    * 0. Sample only: Parse command line arguments
+    **************************************/
     @Argument(help: "The endpoint to connect to.")
     var endpoint: String
 
@@ -29,82 +34,99 @@ struct PKCS12Sample: ParsableCommand {
     @Argument(help: "Password for the pkcs12 file.")
     var pkcs12Password: String
     
-    @Option(help: "Client id to use (optional). Please make sure the client id matches the policy.")
-    var clientId: String? = "test-" + UUID().uuidString
+    @Argument(help: "Client id to use (optional). Please make sure the client id matches the policy.")
+    var clientId: String = "test-" + UUID().uuidString
     
     // The main function to run
     mutating func run() throws {
+        // We use DispatchSemaphore in the sample to wait for various lifecycle events before proceeding.
+        // You would not typically use them in this manner in your own production code.
+        let connectionSemaphore = DispatchSemaphore(value: 0)
+        let stoppedSemaphore = DispatchSemaphore(value: 0)
+        
         /**************************************
-         * 1. Init library
+         * 1. Initialize Device Sdk library
          **************************************/
+        // The IoT Device SDK must be initialized before it is used.
         IotDeviceSdk.initialize();
-        // Uncomment the following line to init debug log to help with debugging.
-        try? Logger.initialize(target: .standardOutput, level: .debug)
+
+        // Uncomment the following line to initialize logging to standard output.
+        // try? Logger.initialize(target: .standardOutput, level: .debug)
         
         do {
             /**************************************
-             * 2. Setup Callbacks
+             * 2. Create Mqtt5ClientBuilder 
              **************************************/
-             // Prepare client callbacks
+            // Create an Mqtt5ClientBuilder configured to connect using PKCS12
+            let clientBuilder = try Mqtt5ClientBuilder.mtlsFromPKCS12(pkcs12Path: pkcs12Path, pkcs12Password: pkcs12Password, endpoint: endpoint)
+
+
+            /**************************************
+             * 3. Setup Callbacks and other options
+             **************************************/
+            // Callbacks to be assigned to builder
+            // The full list of callbacks and their uses can be found in the MQTT5 User Guide
             func onLifecycleEventStopped(_: LifecycleStoppedData) async -> Void {
                 print("Mqtt5Client: onLifecycleEventStopped callback invoked.")
+                stoppedSemaphore.signal()
             }
             func onLifecycleEventAttemptingConnect(_: LifecycleAttemptingConnectData) async -> Void {
                 print("Mqtt5Client: onLifecycleEventAttemptingConnect callback invoked.")
             }
             func onLifecycleEventConnectionSuccess(_ : LifecycleConnectionSuccessData) async -> Void {
                 print("Mqtt5Client: onLifecycleEventConnectionSuccess callback invoked.")
+                connectionSemaphore.signal()
             }
             func onLifecycleEventConnectionFailure(failureData: LifecycleConnectionFailureData) async -> Void {
                 print("Mqtt5Client: onLifecycleEventConnectionFailure callback invoked with Error Code \(failureData.crtError.code): \(failureData.crtError.message)")
             }
             func onLifecycleEventDisconnection(disconnectionData: LifecycleDisconnectData) async -> Void {
                 print("Mqtt5Client: onLifecycleEventDisconnection callback invoked with Error Code \(disconnectionData.crtError.code): \(disconnectionData.crtError.message)")
-            }
-            // Setup the test context which handles the callbacks, the callback is using DispatchSemaphore for demo purpose.
-            // You probably do not want to wait on DispatchSemaphore for a final product.
-            let mqtt5ClientCallbacks: Mqtt5ClientCallbacks = Mqtt5ClientCallbacks()
-                    
-            // Create a client builder to help setup the mqtt client
-            let clientBuilder = try Mqtt5ClientBuilder.MTLSFromPKCS12(pkcs12Path: pkcs12Path, pkcs12Password: pkcs12Password, endpoint: endpoint)
+            }                                
 
-            // Setup callbacks
+            // Callbacks can be assigned all at once using `withCallbacks` on the Mqtt5ClientBuilder
             clientBuilder.withCallbacks(onLifecycleEventAttemptingConnect: onLifecycleEventAttemptingConnect,
                                         onLifecycleEventConnectionSuccess: onLifecycleEventConnectionSuccess,
                                         onLifecycleEventConnectionFailure: onLifecycleEventConnectionFailure,
                                         onLifecycleEventDisconnection: onLifecycleEventDisconnection,
                                         onLifecycleEventStopped: onLifecycleEventStopped)
+
+            // They can also be assinged individually
+            clientBuilder.withOnLifecycleEventConnectionSuccess(onLifecycleEventConnectionSuccess)            
             
-            // setup client id if client id is passed as argument
-            if let _clientId = self.clientId {
-                clientBuilder.withClientId(_clientId);
-            }
-            
+            // Various other configuration options can be set on the Mqtt5ClientBuilder.
+            clientBuilder.withClientId(clientId);
 
             /**********************************************
-             * 3. Create Mqtt Client with Mqtt5ClientBuilder
+             * 4. Create an Mqtt5 Client with Mqtt5ClientBuilder
              ***********************************************/
             let client = try clientBuilder.build()
             
             
             /**************************************
-             * 4. Start the connection session
+             * 5. Start the connection session
              **************************************/
+            // `start()` will put the Mqtt5 Client in a state that desires to be connected. A connection attempt will be made.
+            // If an attempt fails, the client will continue to attempt connections until it is instructed to `stop()`.
             try client.start()
 
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                print("This runs after a 2-second delay.")
-            }
+            // Wait for a successful connection before proceeding with the sample.
+            connectionSemaphore.wait()
+            
             
             /**************************************
-             * 5. Stop the connection session
+             * 6. Stop the connection session
              **************************************/
+            // `stop()` will put the Mqtt5 Client in a state that desires to be disconnected. If in a connected state, the client
+            // will disconnect and not attempt to connect until it is instructed to `start()`.
             try client.stop()
-            
-            
+
+            // Wait for the client to be stopped before exiting the sample.
+            stoppedSemaphore.wait()
+
+            print("Sample complete.")
         } catch {
             print("Failed to setup client.")
         }
-        
     }
 }
