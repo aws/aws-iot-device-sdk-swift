bd25072cb9d73592e415d470b8649342d8819df0
Steve Kim <sbstevek@amazon.com>
2025-02-14 13:20:28 -0800

    refactored x509 sample

--- Samples/Mqtt5ConnectionSamples/X509Connect/Sources/main.swift
+++ Samples/Mqtt5ConnectionSamples/X509Connect/Sources/main.swift
@@ -7,15 +7,15 @@ import ArgumentParser
 
 // This sample shows how to create a mTLS MQTT connection session using the X509 certificate file and key file.
 // Here is the steps to setup a client and connection
-// 0. Parsing command line arguments
-// 1. Init library
-// 2. Setup Connect Options & callbacks
-// 3. Create Mqtt Client with MqttClientBuilder
+// 0. Sample only: Parse command line arguments
+// 1. Initialize Device Sdk library
+// 2. Create Mqtt5ClientBuilder 
+// 3. Setup Callbacks and other options
 // 4. Start the connection session
 // 5. Stop the connection session
 
 /**************************************
- * 0. Parsing command line arguments
+ * 0. Sample only: Parse command line arguments
  * Setup commandline arguments
  **************************************/
 @main
@@ -29,112 +29,94 @@ struct X509MTLSConnectSample: ParsableCommand {
     @Argument(help: "The path to the private key file.")
     var key: String
     
-    @Option(help: "The path to the override root CA file (optional).")
-    var ca_file: String? = nil
-    
-    @Option(help: "Client id to use (optional). Please make sure the client id matches the policy.")
+    @Argument(help: "Client id to use (optional). Please make sure the client id matches the policy.")
     var client_id: String? = "test-" + UUID().uuidString
     
     // The main function to run
     mutating func run() throws {
+        // We use DispatchSemaphore in the sample to wait for various lifecycle events before proceeding.
+        // You would not typically use them in this manner in your own production code.
+        let connectionSemaphore = DispatchSemaphore(value: 0)
+        let stoppedSemaphore = DispatchSemaphore(value: 0)
+
         /**************************************
-         * 1. Init library
+         * 1. Initialize Device Sdk library
          **************************************/
-        CommonRuntimeKit.initialize();
-        // Uncomment the following line to init debug log to help with debugging.
-        try? Logger.initialize(target: .standardOutput, level: .debug)
+        // The IoT Device SDK must be initialized before it is used.
+        IotDeviceSdk.initialize();
         
         do {
             /**************************************
-             * 2. Setup Connect Options & callbacks
+             * 2. Create Mqtt5ClientBuilder 
              **************************************/
-            // Create and config a client builder to access credentials from file path parsed from command line
-            let sample_context = X509MTLSConnectContext()
+            // Create an Mqtt5ClientBuilder configured to connect using a certificate and private key.
             let clientBuilder = try Mqtt5ClientBuilder.mtlsFromPath(certPath: self.cert, keyPath: self.key, endpoint: self.endpoint)
-            
-            // Setup callbacks and other client options
-            clientBuilder.withCallbacks(onLifecycleEventAttemptingConnect: sample_context.onLifecycleEventAttemptingConnect,
-                                        onLifecycleEventConnectionSuccess: sample_context.onLifecycleEventConnectionSuccess,
-                                        onLifecycleEventConnectionFailure: sample_context.onLifecycleEventConnectionFailure,
-                                        onLifecycleEventDisconnection: sample_context.onLifecycleEventDisconnection,
-                                        onLifecycleEventStopped: sample_context.onLifecycleEventStopped)
-            
-            // setup client id if client id is passed as argument
-            if let _client_id = self.client_id {
-                clientBuilder.withClientId(_client_id);
+
+            /**************************************
+             * 3. Setup Callbacks and other options
+             **************************************/
+            // Callbacks to be assigned to builder
+            // The full list of callbacks and their uses can be found in the MQTT5 User Guide
+            func onLifecycleEventStopped(_: LifecycleStoppedData) async -> Void {
+                print("Mqtt5Client: onLifecycleEventStopped callback invoked.")
+                stoppedSemaphore.signal()
             }
+            func onLifecycleEventAttemptingConnect(_: LifecycleAttemptingConnectData) async -> Void {
+                print("Mqtt5Client: onLifecycleEventAttemptingConnect callback invoked.")
+            }
+            func onLifecycleEventConnectionSuccess(_ : LifecycleConnectionSuccessData) async -> Void {
+                print("Mqtt5Client: onLifecycleEventConnectionSuccess callback invoked.")
+                connectionSemaphore.signal()
+            }
+            func onLifecycleEventConnectionFailure(failureData: LifecycleConnectionFailureData) async -> Void {
+                print("Mqtt5Client: onLifecycleEventConnectionFailure callback invoked with Error Code \(failureData.crtError.code): \(failureData.crtError.message)")
+            }
+            func onLifecycleEventDisconnection(disconnectionData: LifecycleDisconnectData) async -> Void {
+                print("Mqtt5Client: onLifecycleEventDisconnection callback invoked with Error Code \(disconnectionData.crtError.code): \(disconnectionData.crtError.message)")
+            }                                
+
+            // Callbacks can be assigned all at once using `withCallbacks` on the Mqtt5ClientBuilder
+            clientBuilder.withCallbacks(onLifecycleEventAttemptingConnect: onLifecycleEventAttemptingConnect,
+                                        onLifecycleEventConnectionSuccess: onLifecycleEventConnectionSuccess,
+                                        onLifecycleEventConnectionFailure: onLifecycleEventConnectionFailure,
+                                        onLifecycleEventDisconnection: onLifecycleEventDisconnection,
+                                        onLifecycleEventStopped: onLifecycleEventStopped)
+
+            // They can also be assinged individually
+            clientBuilder.withOnLifecycleEventConnectionSuccess(onLifecycleEventConnectionSuccess)            
+            
+            // Various other configuration options can be set on the Mqtt5ClientBuilder.
+            clientBuilder.withClientId(clientId);
             
             /**********************************************
-             * 3. Create Mqtt Client with Mqtt5ClientBuilder
+             * 3. Create Mqtt5 Client with Mqtt5ClientBuilder
              ***********************************************/
             let client = try clientBuilder.build()
             
             
             /**************************************
-             * 4. Start the connection session
+             * 5. Start the connection session
              **************************************/
+            // `start()` will put the Mqtt5 Client in a state that desires to be connected. A connection attempt will be made.
+            // If an attempt fails, the client will continue to attempt connections until it is instructed to `stop()`.
             try client.start()
-            if sample_context.semaphoreConnectionSuccess.wait(timeout: .now() + 5) == .timedOut {
-                print("Client start failed to connect after 5 seconds")
-            }
+
+            // Wait for a successful connection before proceeding with the sample.
+            connectionSemaphore.wait()
             
             /**************************************
-             * 5. Stop the connection session
+             * 6. Stop the connection session
              **************************************/
+            // `stop()` will put the Mqtt5 Client in a state that desires to be disconnected. If in a connected state, the client
+            // will disconnect and not attempt to connect until it is instructed to `start()`.
             try client.stop()
-            if sample_context.semaphoreStopped.wait(timeout: .now() + 5) == .timedOut {
-                print("Client stop failed after 5 seconds")
-            }
-            
-            
+
+            // Wait for the client to be stopped before exiting the sample.
+            stoppedSemaphore.wait()
+
+            print("Sample complete.")
         } catch {
             print("Failed to setup client.")
         }
-        
-    }
-}
-
-
-    /**************************************
-     * Setup client callbacks
-     **************************************/
-struct X509MTLSConnectContext{    
-    // We wait on semaphore to demonstrate the client features. However, you probably do not want to block the application in a real product.
-    var semaphoreConnectionSuccess: DispatchSemaphore
-    var semaphoreConnectionFailure: DispatchSemaphore
-    var semaphoreDisconnection: DispatchSemaphore
-    var semaphoreStopped: DispatchSemaphore
-    
-    // Prepare client callbacks
-    func onLifecycleEventStopped(_: LifecycleStoppedData) async -> Void
-    {
-        print("Mqtt5ClientTests: onLifecycleEventStopped")
-        semaphoreStopped.signal()
-    }
-    
-    func onLifecycleEventAttemptingConnect(_: LifecycleAttemptingConnectData) async -> Void {
-        print("Mqtt5ClientTests: onLifecycleEventAttemptingConnect")
-    }
-    func onLifecycleEventConnectionSuccess(_ : LifecycleConnectionSuccessData) async -> Void
-    {
-        print("Mqtt5ClientTests: onLifecycleEventConnectionSuccess")
-        semaphoreConnectionSuccess.signal()
-    }
-    func onLifecycleEventConnectionFailure(failureData: LifecycleConnectionFailureData) async -> Void
-    {
-        print("Mqtt5ClientTests: onLifecycleEventConnectionFailure: Error Code \(failureData.crtError.code): \(failureData.crtError.message)")
-        semaphoreConnectionFailure.signal()
-    }
-    func onLifecycleEventDisconnection(disconnectionData: LifecycleDisconnectData) async -> Void
-    {
-        print("Mqtt5ClientTests: onLifecycleEventDisconnection:  Error Code \(disconnectionData.crtError.code): \(disconnectionData.crtError.message)")
-        semaphoreDisconnection.signal()
-    }
-    
-    init() {
-        semaphoreConnectionSuccess = DispatchSemaphore(value: 0)
-        semaphoreConnectionFailure  = DispatchSemaphore(value: 0)
-        semaphoreDisconnection = DispatchSemaphore(value: 0)
-        semaphoreStopped = DispatchSemaphore(value: 0)
     }
 }
