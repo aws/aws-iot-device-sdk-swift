ffdb14f0ef65594cec4731d1c6cd3962866ac8af
Vera Xia <zhvxia@amazon.com>
2025-03-14 16:12:18 -0700

    Cognito Crednetial Provider Support (#6)
    * groundwork files
    
    * builder WIP
    
    * typo
    
    * ignore Package.resolved
    
    * delete Package.resolved
    
    * change package name to match others
    
    * change tests to point to new name
    
    * tie up name change
    
    * wip on default aws signing
    
    * builder inits and creation funcs implemented
    
    * remove port optional argument from builders
    
    * try to expose CRT public classes/funcs
    
    * connect sample
    
    * WIP ci wip
    
    * add ci script
    
    * fix ci.yaml
    
    * update permission
    
    * fix working path
    
    * checkout source
    
    * enable python env
    
    * direct install boto3
    
    * make sure checkout first
    
    * enable logs
    
    * setup client id
    
    * redirect output
    
    * redirect to pipe
    
    * set package version to 5.8
    
    * update readme
    
    * improve readme
    
    * remaining properties for builder
    
    * add cognito unit test
    
    * add cognito provider connect sample
    
    * setup ci and fix sample
    
    * fix product name
    
    * fix cognito endpoint
    
    * add readme
    
    * improve read me
    
    * remove port setting in static builder functions
    
    * mve helper function outside of builder class
    
    * fix pkcs12 params
    
    * hold on building tls ctx till build is called
    
    * builder CA overrides
    
    * add an optional bootstrap argument to DefaultAwsSigning builder func
    
    * add the bootstrap arg to websocketsWithDefaultAwsSigning
    
    * rearrange and add setting of Secitem Labels
    
    * bootstrap is optional
    
    * add test
    
    * http proxy test can only be run in codebuild
    
    * added initialization for Iot Device SDK to prevent need to initialize the CommonRuntimeKit
    
    * capitalization
    
    * fix merge
    
    ---------
    
    Co-authored-by: Steve Kim <sbstevek@amazon.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["AwsIotDeviceSdkSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "secitem_bindings"), // DEBUG WIP change 'branch' to `from: "aws-crt-swift version number"` when crt is updated.
+        .package(url: "https://github.com/awslabs/aws-crt-swift.git", branch: "cognito_support"), // DEBUG WIP change 'branch' to `from: "aws-crt-swift version number"` when crt is updated.
     ],
     targets: [
         .target(
