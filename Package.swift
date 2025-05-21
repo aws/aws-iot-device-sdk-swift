f8b0623704bbcdcafa9479cd741fd772494122b0
Steve Kim <sbstevek@amazon.com>
2025-05-21 08:32:24 -0700

    update swift-tools-version to 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -31,6 +31,6 @@ let package = Package(
             name: "AwsIotDeviceSdkSwiftTests",
             dependencies: ["AwsIotDeviceSdkSwift"],
             path: "Tests/AwsIotDeviceSdkSwiftTests"
-        )
+        ),
     ]
 )
