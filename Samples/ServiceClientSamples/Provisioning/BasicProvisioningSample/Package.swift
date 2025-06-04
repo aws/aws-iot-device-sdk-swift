ff35e4c642d65295c20c4ea54b4de597dd56fd72
Steve Kim <sbstevek@amazon.com>
2025-06-04 14:31:13 -0700

    swift-format new files

--- Samples/ServiceClientSamples/Provisioning/BasicProvisioningSample/Package.swift
+++ Samples/ServiceClientSamples/Provisioning/BasicProvisioningSample/Package.swift
@@ -4,30 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "BasicProvisioningSample",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v12),
-        .tvOS(.v13),
-    ],
-    products: [
-        .executable(name: "BasicProvisioningSample", targets: ["BasicProvisioningSample"])
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/aws/aws-iot-device-sdk-swift",
-            branch: "codegen"),  // TODO WIP Use the correct URL for your AWS IoT Device SDK Swift repo
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),  // This package gives us the capability to do a argument parsing
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "BasicProvisioningSample",
-            dependencies: [
-                .product(name: "IotIdentityClient", package: "aws-iot-device-sdk-swift"),
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ],
-            path: "Sources")
-    ]
+  name: "BasicProvisioningSample",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v12),
+    .tvOS(.v13),
+  ],
+  products: [
+    .executable(name: "BasicProvisioningSample", targets: ["BasicProvisioningSample"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/aws/aws-iot-device-sdk-swift",
+      branch: "codegen"),  // TODO WIP Use the correct URL for your AWS IoT Device SDK Swift repo
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),  // This package gives us the capability to do a argument parsing
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "BasicProvisioningSample",
+      dependencies: [
+        .product(name: "IotIdentityClient", package: "aws-iot-device-sdk-swift"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      path: "Sources")
+  ]
 )
