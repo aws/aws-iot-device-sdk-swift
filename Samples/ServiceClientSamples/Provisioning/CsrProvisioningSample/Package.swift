19d2d70a58a966deb0cd9ade3eac89b864549320
Steve Kim <sbstevek@amazon.com>
2025-06-04 14:38:18 -0700

    merge and fixes

--- Samples/ServiceClientSamples/Provisioning/CsrProvisioningSample/Package.swift
+++ Samples/ServiceClientSamples/Provisioning/CsrProvisioningSample/Package.swift
@@ -16,8 +16,10 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/aws/aws-iot-device-sdk-swift",
-      branch: "codegen"),  // TODO WIP Use the correct URL for your AWS IoT Device SDK Swift repo
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),  // This package gives us the capability to do a argument parsing
+      // TODO WIP Use the correct URL for your AWS IoT Device SDK Swift repo
+      branch: "codegen"),
+    // This package gives us the capability to do a argument parsing
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
