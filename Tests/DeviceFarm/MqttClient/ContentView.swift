c1f9898e9c1d575843eba68633fe0b361ff49b4d
Vera Xia <zhvxia@amazon.com>
2025-07-02 13:20:55 -0700

    fix team id and target version

--- Tests/DeviceFarm/MqttClient/ContentView.swift
+++ Tests/DeviceFarm/MqttClient/ContentView.swift
@@ -4,16 +4,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+      Text("Hello, world!")
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
