a3e22961bfd2e38bb0b1bb1286e0240099779807
Vera Xia <zhvxia@amazon.com>
2025-07-31 14:22:52 -0700

    remove debug print

--- Scripts/DeviceFarmScripts/run_ios_ci.py
+++ Scripts/DeviceFarmScripts/run_ios_ci.py
@@ -134,7 +134,6 @@ def main():
     if get_run_response['run']['result'] != 'PASSED':
         print('run has failed with result ' +
               get_run_response['run']['result'])
-        print(get_run_response)
         is_success = False
 
     # If Clean up is not executed due to the job being cancelled in CI, the uploaded files will not be deleted
