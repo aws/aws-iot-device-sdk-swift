47b6464d23597f3e1b26c3da8e5cccc1cf92b965
Vera Xia <zhvxia@amazon.com>
2025-08-27 09:35:47 -0700

    Update CRT to 0.54.0 (#45)
    * update crt to 0.54.0
    
    * add comments for version update
    
    * lint
    
    * update ci to use xcode16.3 and ios18.4
    
    * remove xcode16 from macos14-large

--- Package.swift
+++ Package.swift
@@ -26,10 +26,23 @@ let package = Package(
   ],
   dependencies: [
     .package(
-      url: "https://github.com/awslabs/aws-crt-swift.git", .upToNextMajor(from: "0.53.0")),
+      url: "https://github.com/awslabs/aws-crt-swift.git", .upToNextMajor(from: "0.54.0")),
     // aws-sdk-swift is only used in test targets to help with setup and cleanup of testing service clients
     // We use "aws-iot-device-sdk-swift-testing-branch" to maintain aws-crt-swift version pairity between it
-    // and our SDK for testing
+    // and our SDK for testing. As aws-sdk-swift depends on swift-smithy, which also uses aws-crt-swift, we
+    // keep the versions in sync using a fork https://github.com/sbSteveK/smithy-swift
+    // Steps for updating the versions:
+    //    1. Update crt version for smithy-swift
+    //        a. fork from https://github.com/sbSteveK/smithy-swift.git
+    //        b. pull in latest https://github.com/smithy-lang/smithy-swift and update aws-crt-swift version in `Package.swift`
+    //        c. create a PR to sbSteveK/smithy, and submit for @sbstevek to review and merge
+    //        d. go to latest Package.swift from awslabs/aws-crt-swift, and look for `clientRuntimeVersion`. This is the version tag of smithy-swift that aws-sdk-swift is using.
+    //        e. cut a release in sbSteveK/smithy using the same version tag as `clientRuntimeVersion`
+    //    2. Update crt version for https://github.com/awslabs/aws-sdk-swift
+    //        a. branch from "aws-iot-device-sdk-swift-testing-branch"
+    //        b. pull in the latest main and update crt version as needed
+    //        c. submit PR against "aws-iot-device-sdk-swift-testing-branch" for review and merge
+    // Once the PRs are merged, you can update the version here for aws-crt-swift, remember to test locally to make sure the Package.swift resolves correctly.
     .package(
       url: "https://github.com/awslabs/aws-sdk-swift.git",
       branch: "aws-iot-device-sdk-swift-testing-branch"),
