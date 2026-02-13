946f5163156f0c087fc6ad0754b2a305d77d68e1
Vera Xia <zhvxia@amazon.com>
2026-02-13 13:22:24 -0800

    update swift argument parser version for CI

--- Samples/Mqtt5Sample/Package.swift
+++ Samples/Mqtt5Sample/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   dependencies: [
     .package(path: "../../"),
     // This package gives us the capability to do a argument parsing
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.5.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
